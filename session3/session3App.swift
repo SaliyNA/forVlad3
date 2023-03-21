68a35b9ad5b9c150103ffc22bf9be4bbf2037b0c
nke <nke@nke.ru>
2023-03-21 22:47:53 +0700

    Initial Commit

---
+++ session3/session3App.swift
@@ -0,0 +1,17 @@
+//
+//  session3App.swift
+//  session3
+//
+//  Created by admin on 21.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct session3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
